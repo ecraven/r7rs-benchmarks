77d8362a2c73df681d595a5a3904532dab6c617d
Peter <craven@gmx.net>
2016-05-04 20:21:18 +0200

    Switch Racket to run racket, not plt-r6rs

--- src/Racket-prelude.scm
+++ src/Racket-prelude.scm
@@ -1,5 +1,6 @@
-(import (rnrs) (rnrs mutable-pairs (6)) (rnrs lists) (rnrs r5rs)
-        (only (racket) current-seconds current-inexact-milliseconds flush-output version))
+#lang racket
+;; (import (rnrs) (rnrs mutable-pairs (6)) (rnrs lists) (rnrs r5rs)
+;;         (only (racket) current-seconds current-inexact-milliseconds flush-output version))
 (define flush-output-port flush-output)
 (define current-jiffy current-inexact-milliseconds)
 (define (jiffies-per-second) 1000)
@@ -7,3 +8,5 @@
 (define (square x) (* x x))
 (define (this-scheme-implementation-name)
   (string-append "racket-" (version)))
+(define inexact exact->inexact)
+(define exact inexact->exact)
