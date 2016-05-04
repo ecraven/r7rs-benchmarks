bc141893b21c995d85993fc91e57b1c63b294b7e
Peter <craven@gmx.net>
2016-05-04 20:49:16 +0200

    Change racket back to plt-r6rs

--- src/Racket-prelude.scm
+++ src/Racket-prelude.scm
@@ -1,6 +1,5 @@
-#lang racket
-;; (import (rnrs) (rnrs mutable-pairs (6)) (rnrs lists) (rnrs r5rs)
-;;         (only (racket) current-seconds current-inexact-milliseconds flush-output version))
+(import (rnrs) (rnrs mutable-pairs (6)) (rnrs lists) (rnrs r5rs)
+        (only (racket) current-seconds current-inexact-milliseconds flush-output version))
 (define flush-output-port flush-output)
 (define current-jiffy current-inexact-milliseconds)
 (define (jiffies-per-second) 1000)
