62c81ad8f5bbe85a861aee7a58af8a422eed9ccd
Peter <craven@gmx.net>
2016-05-04 20:32:19 +0200

    Fix Bigloo

---
+++ src/Bigloo-prelude.scm
@@ -0,0 +1,12 @@
+(module test)
+(define current-second
+  current-seconds)
+(define (jiffies-per-second) 1000000000)
+(define (current-jiffy)
+  (llong->flonum (current-nanoseconds)))
+(define (this-scheme-implementation-name)
+  (string-append "bigloo-" (bigloo-config 'release-number)))
+(define inexact exact->inexact)
+(define exact inexact->exact)
+(define call-with-current-continuation call/cc)
+#;
