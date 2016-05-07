ad6f83f2c0767aedd94a99386f543331b3907b90
Peter <craven@gmx.net>
2016-05-07 09:28:29 +0200

    Add missing procedures as comments to Bones prelude

--- src/Bones-prelude.scm
+++ src/Bones-prelude.scm
@@ -5,5 +5,10 @@
 (define exact inexact->exact)
 (define (square n) (* n n))
 (define (jiffies-per-second)
-  1000000000 ;; returns 1 on my bones, which is wrong. this number should work for ?many? linuxen
+  1000000000 ;; returns 1 on my Bones, which is wrong. this number should work for ?many? linuxen
   )
+(define exact-integer? integer?)
+;; vector-map
+;; read-line
+;; complex/rational functions
+;; define-record-type
