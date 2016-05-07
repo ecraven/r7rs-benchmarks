fdf8d33a96667e47f0d634e87fc0c6357be94042
Peter <craven@gmx.net>
2016-05-07 09:10:19 +0200

    Add comments to GambitC-prelude on syntax-rules

--- src/GambitC-prelude.scm
+++ src/GambitC-prelude.scm
@@ -1,8 +1,3 @@
-;; (define-syntax import
-;;   (syntax-rules ()
-;;     ((import stuff ...)
-;;      (begin) ;; do nothing
-;;      )))
 (define (current-second) (truncate (current-jiffy)))
 (define (jiffies-per-second) 1)
 (define (current-jiffy) (time->seconds (current-time)))
@@ -11,3 +6,11 @@
 (define inexact exact->inexact)
 
 (define (this-scheme-implementation-name) (string-append "gambitc-" (system-version-string)))
+;; TODO: load syntax-case here, to get syntax-rules.
+;; google says (load "~~/syntax-case"), but that doesn't work on my machine :-/
+
+;; (define-syntax import
+;;   (syntax-rules ()
+;;     ((import stuff ...)
+;;      (begin) ;; do nothing
+;;      )))
