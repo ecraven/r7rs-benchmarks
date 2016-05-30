8911f79c6c281ab5532200cbdc58f0793d181ba4
Michael Lenaghan <michaell@dazzit.com>
2016-05-30 19:35:01 -0400

    Strict R6RS mode (which we're now using) doesn't allow symbols that start with digits.

--- src/Petite-postlude.scm
+++ src/Petite-postlude.scm
@@ -1,11 +1,11 @@
 (define (this-scheme-implementation-name)
   (let* ((machine-type-name (symbol->string (machine-type)))
          (threads (if (char=? (string-ref machine-type-name 0) #\t)
-                     'multithreaded
-                     'singlethreaded))
-         (bits (if (char=? (string-ref machine-type-name (if (eq? threads 'multithreaded) 2 1)) #\6)
-                   '64bit
-                   '32bit)))
+                     "multithreaded"
+                     "singlethreaded"))
+         (bits (if (char=? (string-ref machine-type-name (if (string=? threads "multithreaded") 2 1)) #\6)
+                   "64bit"
+                   "32bit")))
     (string-append "chez-" (call-with-values scheme-version-number
                              (lambda (a b c)
                                (string-append (number->string a)
@@ -13,8 +13,8 @@
                                               (number->string b)
                                               "."
                                               (number->string c))))
-                            "-" (symbol->string bits)
-                            "-" (symbol->string threads))))
+                            "-" bits
+                            "-" threads)))
 (define (current-second)
   (time-second (current-time)))
 (define (current-jiffy)
