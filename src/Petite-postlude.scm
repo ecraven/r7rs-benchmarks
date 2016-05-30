4680a5a5deef759511f67cbbe70133a3b6e39976
Michael Lenaghan <michaell@dazzit.com>
2016-05-30 13:03:12 -0400

    Add 32bit/64bit and singlethreaded/multithreaded to Chez/Petite implementation name.
    On almost all platforms, Chez can be built four ways:
    
     * 32-bit single-threaded
     * 32-bit multi-threaded
     * 64-bit single-threaded
     * 64-bit multi-threaded
    
    Since the variant that's used can have a significant impact on performance this change makes the variant in use part of the implementation name.

--- src/Petite-postlude.scm
+++ src/Petite-postlude.scm
@@ -1,11 +1,20 @@
 (define (this-scheme-implementation-name)
-  (string-append "petite-" (call-with-values scheme-version-number
-                           (lambda (a b c)
-                             (string-append (number->string a)
-                                            "."
-                                            (number->string b)
-                                            "."
-                                            (number->string c))))))
+  (let* ((machine-type-name (symbol->string (machine-type)))
+         (threads (if (char=? (string-ref machine-type-name 0) #\t)
+                     'multithreaded
+                     'singlethreaded))
+         (bits (if (char=? (string-ref machine-type-name (if (eq? threads 'multithreaded) 2 1)) #\6)
+                   '64bit
+                   '32bit)))
+    (string-append "chez-" (call-with-values scheme-version-number
+                             (lambda (a b c)
+                               (string-append (number->string a)
+                                              "."
+                                              (number->string b)
+                                              "."
+                                              (number->string c))))
+                            "-" (symbol->string bits)
+                            "-" (symbol->string threads))))
 (define (current-second)
   (time-second (current-time)))
 (define (current-jiffy)
