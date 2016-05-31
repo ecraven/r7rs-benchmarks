f974fe1b686baac15344698e2c5c0a91b31967a4
Michael Lenaghan <michaell@dazzit.com>
2016-05-31 09:36:21 -0400

    Change the way variants are presented (e.g., "...-64bit-multithreaded" => "...-m64").

--- src/Chez-postlude.scm
+++ src/Chez-postlude.scm
@@ -1,11 +1,11 @@
 (define (this-scheme-implementation-name)
   (let* ((machine-type-name (symbol->string (machine-type)))
          (threads (if (char=? (string-ref machine-type-name 0) #\t)
-                     "multithreaded"
-                     "singlethreaded"))
-         (bits (if (char=? (string-ref machine-type-name (if (string=? threads "multithreaded") 2 1)) #\6)
-                   "64bit"
-                   "32bit")))
+                     "m"
+                     "s"))
+         (bits (if (char=? (string-ref machine-type-name (if (string=? threads "m") 2 1)) #\6)
+                   "64"
+                   "32")))
     (string-append "chez-" (call-with-values scheme-version-number
                              (lambda (a b c)
                                (string-append (number->string a)
@@ -13,8 +13,7 @@
                                               (number->string b)
                                               "."
                                               (number->string c))))
-                            "-" bits
-                            "-" threads)))
+                            "-" threads bits)))
 (define (current-second)
   (time-second (current-time)))
 (define (current-jiffy)
