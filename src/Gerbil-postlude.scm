1b83735ca5c307c523e44ab66b1937d3066a0be9
Peter <craven@gmx.net>
2018-10-03 11:53:33 +0200

    add missing import to gerbil postlude

--- src/Gerbil-postlude.scm
+++ src/Gerbil-postlude.scm
@@ -1,3 +1,3 @@
-(import (only (gerbil core) gerbil-version-string))
+(import (only (gerbil core) gerbil-version-string string-split))
 (define (this-scheme-implementation-name)
   (string-append "gerbil-" (car (string-split (gerbil-version-string) #\-))))
