d2746d28a26e6679f1b22cc573679b46da56f829
Vasilij Schneidermann <mail@vasilij.de>
2018-10-24 09:15:06 +0200

    Improve CHICKEN coverage (#36)
    * Add missing bv2string procedures
    
    * Use modules and redefine write-string for tail

--- src/Chicken-prelude.scm
+++ src/Chicken-prelude.scm
@@ -1,4 +1,5 @@
-(use extras) ;; for read-line
+(import chicken scheme srfi-4 lolevel)
+(use (rename extras (write-string %write-string))) ;; for read-line, write-string
 (use vector-lib) ;; for vector-map
 (define flush-output-port flush-output)
 (define-syntax import
@@ -13,5 +14,23 @@
 (define exact inexact->exact)
 (define (square x) (* x x))
 (define exact-integer? integer?)
+
+;; tail
+(define (write-string string #!optional out)
+  (%write-string string #f out))
+
+;; bv2string
+(define make-bytevector make-u8vector)
+(define bytevector-u8-set! u8vector-set!)
+
+(define (string->utf8 string)
+  (let ((u8vector (make-u8vector (string-length string))))
+    (move-memory! string u8vector)
+    u8vector))
+
+(define (utf8->string u8vector)
+  (let ((string (make-string (u8vector-length u8vector))))
+    (move-memory! u8vector string)
+    string))
 
 (define (this-scheme-implementation-name) (string-append "chicken-" (chicken-version)))
