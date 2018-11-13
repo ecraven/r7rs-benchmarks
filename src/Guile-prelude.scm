e13f003a8842032dc771c1ddd9e9a4e388bcb7d7
Arne Babenhauserheide <arne_bab@web.de>
2018-11-13 23:36:50 +0100

    Add support for running guile-1.8.8
    run 1.8.8 with GUILE=~/Quell/Programme/guile-1.8.8/guile-1.8.8/pre-inst-guile GUILD=echo bash -x ./bench guile all

--- src/Guile-prelude.scm
+++ src/Guile-prelude.scm
@@ -1,4 +1,5 @@
-(use-modules (ice-9 rdelim))
+(use-modules (ice-9 rdelim)
+             (ice-9 syncase))
 (define-syntax import
   (syntax-rules ()
     ((import stuff ...)
@@ -13,8 +14,10 @@
 (define (square x) (* x x))
 (define (write-string str out) (display str out)) ; sufficient for tail.scm
 (define (this-scheme-implementation-name) (string-append "guile-" (version)))
-(read-enable 'r7rs-symbols)
-(print-enable 'r7rs-symbols)
-(use-modules (rnrs bytevectors)
-	     (rnrs base)
-	     (srfi srfi-9))
+(cond
+  ((string-ci> (version) "2.0")
+   (read-enable 'r7rs-symbols)
+   (print-enable 'r7rs-symbols)
+   (use-modules (rnrs bytevectors)
+                (rnrs base))))
+(use-modules (srfi srfi-9))
