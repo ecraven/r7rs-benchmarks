b475dd8807be486a5f6230ce179eeadf9473ecf9
Peter <craven@gmx.net>
2022-06-19 09:57:21 +0200

    Add Guile3-prelude.

---
+++ src/Guile3-prelude.scm
@@ -0,0 +1,20 @@
+(use-modules (ice-9 rdelim))
+(define-syntax import
+  (syntax-rules ()
+    ((import stuff ...)
+     (begin) ;; do nothing
+     )))
+(define flush-output-port force-output)
+(define current-second current-time)
+(define (jiffies-per-second) internal-time-units-per-second)
+(define current-jiffy get-internal-real-time)
+(define exact inexact->exact)
+(define inexact exact->inexact)
+(define (square x) (* x x))
+(define (write-string str out) (display str out)) ; sufficient for tail.scm
+(define (this-scheme-implementation-name) (string-append "guile3-" (version)))
+(read-enable 'r7rs-symbols)
+(print-enable 'r7rs-symbols)
+(use-modules (rnrs bytevectors)
+	     (rnrs base)
+	     (srfi srfi-9))
