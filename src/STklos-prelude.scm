4621af86109acc89137e4dac40d2f2e23ae10c7b
Jeronimo Pellegrini <j_p@aleph0.info>
2024-07-23 16:53:04 -0300

    Disable colon for keywords in STklos
    By default STklos will recognize '|: as a keyword, not the symbol |:|.
    so we disable keywords (since they're not standard and not used in
    the benchmarks anyway).
    
    This makes STklos finish the 'dynamic' benchmark.

--- src/STklos-prelude.scm
+++ src/STklos-prelude.scm
@@ -3,5 +3,10 @@
     ((import foo bar ...)
      #t)))
 
+;; By default STklos will recognize '|: as a keyword, not the symbol |:|.
+;; so we disable keywords here (since they're not standard and not used in
+;; the benchmarks anyway).
+(keyword-colon-position 'none)
+
 (define (this-scheme-implementation-name)
   (string-append "stklos-" (version)))
